0385c1e1f4034ec4fa8497434e95cac865ca0ae5
Юрий <yuriy.logosha@gmail.com>
2022-11-06 18:03:34 +0200

    Initial commit.

--- src/main/java/com/example/restwebfluxservice/RestWebfluxServiceApplication.java
+++ src/main/java/com/example/restwebfluxservice/RestWebfluxServiceApplication.java
@@ -12,56 +12,8 @@ public class RestWebfluxServiceApplication {
 
     private static final Logger log = LoggerFactory.getLogger(RestWebfluxServiceApplication.class);
 
-
     public static void main(String[] args) {
         SpringApplication.run(RestWebfluxServiceApplication.class, args);
     }
 
-
-//    @Bean
-//    CommandLineRunner verify(ResultCodeClient client) {
-//        return args -> {
-////            String msg = "111";
-////
-////            if (args.length > 0) {
-////                msg = args[0];
-////            }
-////            Integer code = client.getResultCode(msg);
-////            log.info("ResultCode: " + code);
-////
-////            assert code == 111;
-//
-//
-////            WebClient.create("http://localhost:8080/data/create").post()
-//////                    .contentType(MediaType.APPLICATION_JSON)
-////                    .body(BodyInserters.fromValue("{\n" +
-////                            "\"message\":\"12345\",\n" +
-////                            "\"id\":1\n" +
-////                            "}"))
-////                    .retrieve()
-////                    .bodyToMono(String.class)
-////                    .doOnNext(s -> System.out.println(s));
-//
-//            IncomingData m = new IncomingData();
-//            m.setMessage("112233");
-//            m.setId("123");
-//            WebClient.create("http://localhost:8080/data").post()
-//                    .uri("/create")
-//                    .contentType(MediaType.APPLICATION_JSON)
-//                    .body(BodyInserters.fromObject(m))
-//                    .exchange().log();
-////            webClient.post()
-////                    .contentType(MediaType.APPLICATION_JSON)
-////                    .body(BodyInserters.fromValue(body))
-////
-////                    .exchangeToMono(response -> {
-////                        if (response.statusCode().equals(HttpStatus.OK)) {
-////                            return response.bodyToMono(Integer.class);
-////                        } else {
-////                            return response.createException().flatMap(Mono::error);
-////                        }
-////                    });
-//
-//        };
-//    }
 }
