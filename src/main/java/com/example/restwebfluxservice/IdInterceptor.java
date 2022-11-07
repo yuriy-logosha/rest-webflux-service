bb24000cb6d0572a8735f5f088c49a9ae758f41f
Юрий <yuriy.logosha@gmail.com>
2022-11-07 12:57:07 +0200

    Remove IdInterceptor as useless.

--- src/main/java/com/example/restwebfluxservice/IdInterceptor.java
+++
@@ -1,7 +0,0 @@
-package com.example.restwebfluxservice;
-
-import org.springframework.web.servlet.AsyncHandlerInterceptor;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-public class IdInterceptor implements AsyncHandlerInterceptor {
-}
